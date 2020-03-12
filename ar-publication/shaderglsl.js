a21621e4d1034a5629944bdf33d1e917e87683d8
frazermcpherson <62116993+frazermcpherson@users.noreply.github.com>
2020-03-12 22:56:09 +0000

    Create shaderglsl.js

---
+++ ar-publication/shaderglsl.js
@@ -0,0 +1,36 @@
+/* global AFRAME, THREE */
+
+// shader-grid-glitch.js
+
+AFRAME.registerShader('grid-glitch', {
+  schema: {
+    color: {type: 'color', is: 'uniform'},
+    timeMsec: {type: 'time', is: 'uniform'}
+  },
+
+  vertexShader: `
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float time;
+uniform vec2 resolution;
+
+void main() {
+    vec2 p = gl_FragCoord.xy / resolution.x * .05;
+    vec3 col;
+    for(float j = 0.0; j < 3.0; j++){
+        for(float i = 1.; i < 25.0; i++){
+            p.x += (.5*(0.05 / (i + j) * sin(i * 10. * p.y + (time*0.2) + cos((time / (150. * i)) * i + j))));
+            p.y += (1.5*(0.02 / (i + j)* cos(i * 10. * p.x + (time*0.15) + sin((time / (100. * i)) * i + j))));
+        }
+	
+	for(float k = 2.; k < 20.0; k++){
+		col[int(j)] = k*(abs(p.x + p.y));
+	}
+    }
+    gl_FragColor = vec4(col, 1.);
+}
+`
+});
+
